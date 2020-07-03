12c0ec7c6a5990b3e953d8b67785eab283711c03
v.shmyrov <v.shmyrov@kt-team.de>
2020-07-03 14:28:31 +0400

    added some nonfunctional code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
-function do_alert(input) {
-    alert(input.value);
-    var outputElem = document.getElementById('output');
-    outputElem.value = input.value;
+function legitimize(input) {
+    let outputElem = document.getElementById('output');
+    let text = input.value;
+    let regex = /[\wа-я]+/ig;
+    let replacementsArray = Array.from(new Set([...text.matchAll(regex)].flat(1)));
+    let textMatches = [...text.matchAll(regex)];
+    let textArr = Array();
+    for (match of textMatches) {
+        textArr.push(match[0]);
+    }
+
+    textArr.sort((a,b) => {return b.length - a.length})
+    
+    outputElem.innerText = text;
 }
