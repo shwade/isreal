8862060d11f4937d08fcefa9796a995183990291
v.shmyrov <v.shmyrov@kt-team.de>
2020-07-03 15:33:40 +0400

    finally working

--- js/index.js
+++ js/index.js
@@ -3,19 +3,13 @@ function legitimize(input) {
     let text = input.value;
     let regex = /[\wа-я]+/ig;
     let replacementsArray = Array.from(new Set([...text.matchAll(regex)].flat(1)));
-    // let textMatches = [...text.matchAll(regex)];
-    // let textArr = Array();
-    // for (match of textMatches) {
-    //     textArr.push(match[0]);
-    // }
 
     replacementsArray.sort((a,b) => {return b.length - a.length})
 
     for (word of replacementsArray) {
-        let regex = new RegExp(word, "g");
-        text = text.replace(regex, '(((' + word + ')))');
+        let regexRep = new RegExp(word + '\\b', "g");
+        text = text.replace(regexRep, '(((' + word + ')))');
     }
 
     outputElem.innerText = text;
-
 }
